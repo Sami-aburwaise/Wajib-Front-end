6155a578c5b07c36f969d847c5dab42c0dda3959
sami-aburwaise <tarrary1@gmail.com>
2023-11-12 11:55:20 +0300

    question detail page

--- Wajib/src/components/QuestionDetail.jsx
+++ Wajib/src/components/QuestionDetail.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Client, BASE_URL } from '../Globals'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import moment from 'moment'
 
 const QuestionDetail = ({ selectedQuestion }) => {
   const [question, setQuestion] = useState(null)
@@ -18,9 +20,33 @@ const QuestionDetail = ({ selectedQuestion }) => {
   return (
     question && (
       <div id="question-detail">
-        <h2>{question.user.username}</h2>
-        <h2>{question.question}</h2>
-        <h3>{question.subject}</h3>
+        <div id="question-header">
+          <div className="user-tag">
+            <AccountCircleIcon fontSize="large" />
+            <h2>{question.user.username}</h2>
+          </div>
+          <p>{moment(question.createdAt).fromNow()}</p>
+        </div>
+        <hr />
+        <section id="question-container">
+          <div>
+            <h1>Question:</h1>
+            <h1>{question.question}</h1>
+          </div>
+
+          <img
+            src={
+              question.image
+                ? question.image
+                : 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318'
+            }
+            alt=""
+          />
+        </section>
+        <section>
+          <h2>no answered</h2>
+        </section>
+        <section>no comments</section>
       </div>
     )
   )
